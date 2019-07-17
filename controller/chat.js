8baee0ec592656f06632c6383e3351eb3015b239
runsooj <runsooj@gmail.com>
2019-07-17 13:46:30 +0900

    temp

--- controller/chat.js
+++ controller/chat.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 var http = require('http').createServer(app);
+var sessionstorage = require('sessionstorage');
 var io = require('socket.io')(http);
 
 const mysql = require('mysql');
 const auth = require('./authcheck');
 
 router.get('/', (req, res) => {
+  console.log(sessionstorage.getItem('jwtToken'));
+  res.set({'x-access-token':  sessionstorage.getItem('jwtToken')});
   res.render("chat");
 });
 
