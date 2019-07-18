2a75a494085aecfce1c7583b5391c3a85f3340fe
sooji3765 <susie3765@naver.com>
2019-07-18 15:47:12 +0900

    reservation description add

--- controller/reservation.js
+++ controller/reservation.js
@@ -24,19 +24,20 @@ router.post('/check', (req, res) => {
         reservation_start_date,
         reservation_end_date,
         insurance_flag,
-        pay_state
+        pay_state,
+        description
     } = req.body;
 
     console.log(req.body);
 
 
     var sql = "INSERT INTO reservation_info(keeper_id,user_id,reservation_start_date,reservation_end_date" +
-        " ,insurance_flag, product_amount,total_price,pay_state) " +
-        "values(?,?,?,?,?,?,?,?)";
+        " ,insurance_flag, product_amount,total_price,pay_state,description) " +
+        "values(?,?,?,?,?,?,?,?,?)";
 
     connection.query(sql, [keeper_id, user_id, reservation_start_date,
         reservation_end_date, insurance_flag,
-        product_amount, total_price, pay_state
+        product_amount, total_price, pay_state, description
     ], function (err, results) {
 
         if (err) console.error(err);
@@ -81,7 +82,7 @@ router.get('/info/:id', (req, res) => {
     var reservation_id = req.params.id;
 
     var sql = "select r.id, r.keeper_id, r.user_id,r.reservation_start_date, r.reservation_end_date, r.cancel_flag, r.insurance_flag, r.product_amount,r.total_price," +
-        " r.pay_state,k.name,k.address,k.keeper_phone,k.location_lat,k.location_lon" +
+        " r.pay_state,r.description,k.name,k.address,k.keeper_phone,k.location_lat,k.location_lon" +
         " from reservation_info r join keeper k" +
         " on r.keeper_id = k.id" +
         " where r.id = ?;";
