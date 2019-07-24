7b87bcd48400c1c613d9d0002d2381c1efd8c543
runsooj <runsooj@gmail.com>
2019-07-24 15:18:41 +0900

    smart contract

---
+++ config/inbox.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+    
+    function getMessage() public view returns (string) {
+        return message;
+    }
+    
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+}
